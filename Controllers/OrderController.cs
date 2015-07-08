5fecaaf18abbf5a6384170dcb809529612300a7c
hx1982 <26233377@qq.com>
2015-07-08 21:49:17 +0800

    添加违章信息ViewModel

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TCTE.Models;
+using TCTE.ViewModel;
+
+namespace TCTE.Controllers
+{
+    public class OrderController : Controller
+    {
+        //获取违章信息
+        public ActionResult GetPeccancyInfo(string PlateNumber, string VIN)
+        {
+            //模拟数据
+            var peccancyInfo = new PeccancyInfo 
+            { 
+                PlateNumber = "川A12568", Time = DateTime.Now, Address = "锦晖西十街", Behavior = "不按规定停车(1099)", Money = 50, Deduction=0
+            };
+            return View(peccancyInfo);
+        }
+
+        //创建订单
+        public ActionResult Create(Order order)
+        {
+            return View();
+        }
+	}
+}
---
+++ ViewModel/PeccancyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TCTE.ViewModel
+{
+    /// <summary>
+    /// 车辆违法信息
+    /// </summary>
+    public class PeccancyInfo
+    {
+        [Display(Name = "车牌号码")]
+        public string PlateNumber { get; set; }
+
+        [Display(Name = "违法时间"), DisplayFormat(DataFormatString = "{0:yyyy-MM-dd hh:mm:ss}")]
+        public DateTime Time { get; set; }
+
+        [Display(Name = "违法地点")]
+        public string Address { get; set; }
+
+        [Display(Name = "违法行为")]
+        public string Behavior { get; set; }
+
+        [Display(Name = "罚款金额")]
+        public decimal Money { get; set; }
+
+        [Display(Name = "违法扣分")]
+        public int Deduction { get; set; }
+    }
+}
